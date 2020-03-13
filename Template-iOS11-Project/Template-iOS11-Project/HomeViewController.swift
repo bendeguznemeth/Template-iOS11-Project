ff8d9f0be4a448abd9e9d5c48e637efec5654871
Németh Bendegúz <n.bendeguz3@gmail.com>
2020-03-13 08:06:34 +0100

    Project init

---
+++ Template-iOS11-Project/Template-iOS11-Project/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Template-iOS11-Project
+//
+//  Created by Németh Bendegúz on 2020. 03. 12..
+//  Copyright © 2020. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
