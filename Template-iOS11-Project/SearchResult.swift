5db59961f1c53957457b6c20bc885ea832d7558e
Németh Bendegúz <n.bendeguz3@gmail.com>
2020-03-13 16:00:11 +0100

    Added Models

---
+++ Template-iOS11-Project/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  Template-iOS11-Project
+//
+//  Created by Németh Bendegúz on 2020. 03. 13..
+//  Copyright © 2020. Németh Bendegúz. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let results: [Stock]
+}
---
+++ Template-iOS11-Project/Stock.swift
@@ -0,0 +1,17 @@
+//
+//  Stock.swift
+//  Template-iOS11-Project
+//
+//  Created by Németh Bendegúz on 2020. 03. 13..
+//  Copyright © 2020. Németh Bendegúz. All rights reserved.
+//
+
+import Foundation
+
+struct Stock: Codable, Identifiable {
+    let id = UUID()
+    let symbol: String
+    let name: String
+    let lastPrice: Double
+    let percentChange: Double
+}
